42d0625babb4adda8bdf308bc99380a523677413
Aaron Law <aaronlaw@gmail.com>
2015-04-24 14:59:11 +0800

    add home views

--- djangogirls/djangogirls/urls.py
+++ djangogirls/djangogirls/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'trips.views.home'),
     url(r'^hello/$', 'trips.views.hello_world'),
 
 ]
--- djangogirls/trips/views.py
+++ djangogirls/trips/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
+from trips.models import Post
 
 # Create your views here.
 def hello_world(request):
 	data = {'curr_time':datetime.now()}
 	return render(request, 'hello_world.html', data)
+
+def home(request):
+	# get all the posts
+	posts = Post.objects.all()
+	return render(request, 'home.html', {'posts':posts})
